ce7af45c7cb562f06915268203a431d623acecd5
劉晏丞 <P10380916@pic.net.tw>
2023-05-13 14:00:54 +0800

    2023/5/13 AttendSystem Modfiy(Login)

--- AttendSystem/AttendSystem/Models/UserLoginModel.cs
+++ AttendSystem/AttendSystem/Models/UserLoginModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -12,6 +13,13 @@ namespace AttendSystem.Models
     }
     public class UserLoginRequest
     {
+        [Required]
+        [Display(Name ="帳號")]
+        public string account { get; set; }
+
+        [Required]
+        [Display(Name = "密碼")]
+        public string password { get; set; }
 
     }
 }
--- AttendSystem/UserTable.sql
+++ AttendSystem/UserTable.sql
@@ -1,9 +1,10 @@
 ﻿--建立資料庫
 create database AttendSystem
+use AttendSystem
 --建立使用者資料表
 create table UserTable(
 	id int identity(0,1) ,
-	GU_ID guid primary key,
+	GU_ID uniqueidentifier  default NEWID() primary key,
 	name nvarchar(20) not null,
 	account varchar(100) not null,
 	password varchar(100) not null,
@@ -16,3 +17,4 @@ create table UserTable(
 	update_sid int not null,
 	deleted bit default 0
 )
+SELECT * FROM UserTable
